d7561489481e2aab7d295754c64e88de1d7fe82b
ronnie wang <ro_wsy@163.com>
2016-07-20 22:14:54 +0800

    output json result

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,58 +1,71 @@
 import re
-from datetime import datetime
+import time
 import json
+import os.path
 
-line_num = 0
-pre_request_id = 0
+start = time.time()
 
-start = datetime.now().microsecond
+log_file_path = '/Users/ronniewang/tomcat_kc_web_new_2.2016-07-20.log'
+out_file_path = '/Users/ronniewang/out.log'
+if not os.path.isfile(out_file_path):
+    out_file = open(out_file_path, 'a+')
+    with open(log_file_path) as log_file:
+        for line in log_file:
+            match_obj = re.match(r'^.+\[requestId:(\d+)\]', line)
+            if match_obj:
+                match_api_uri = re.match(r'^.+\[(/[0-9a-zA-Z\./]+)[^\]]+\]', line)
+                current_request_id = match_obj.group(1)
 
+                if match_api_uri:
+                    api = match_api_uri.group(1)
+                    request_str = current_request_id, '|api|', api, '\n'
+                    out_file.writelines(request_str)
+
+                    # match_internal_uri = re.match(r'^.+(\[http://[^\]]+\])', line)
+                    # if match_internal_uri:
+                    #     pass
+                    # internal_uri_str = current_request_id, ' internal uri: ', match_internal_uri.group(1), '\n'
+                    # out_file.writelines(internal_uri_str)
+
+                match_net_mills = re.match(r'^.+\[net send mills:(\d+)\]', line)
+                if match_net_mills:
+                    # pass
+                    net_mills_str = current_request_id, '|net_mills|', match_net_mills.group(1), '\n'
+                    out_file.writelines(net_mills_str)
+
+                match_api_mills = re.match(r'^.+\[api mills:(\d+)\]', line)
+                if match_api_mills:
+                    api_mills = match_api_mills.group(1)
+                    # pass
+                    mills_str = current_request_id, '|api_mills|', match_api_mills.group(1), '\n'
+                    out_file.writelines(mills_str)
+
+end = time.time()
+
+print 'time elapsed is ', end - start, 'seconds'
+
+request_map = {}
 api_map = {}
-api_num = 0
-
-with open('/Users/ronniewang/tomcat_kc_web_new_2.2016-07-20.log') as log_file:
-    for line in log_file:
-        line_num += 1
-        match_obj = re.match(r'^.+\[requestId:(\d+)\]', line)
-        if match_obj:
-            match_api_uri = re.match(r'^.+\[(/[0-9a-zA-Z\./]+)[^\]]+\]', line)
-            current_request_id = match_obj.group(1)
-            if current_request_id != pre_request_id:
-                pre_request_id = current_request_id
-                # print '====================================================='
-
-            if match_api_uri:
-                api = match_api_uri.group(1)
-                api_statistic = {}
-                if api_map.has_key(api):
-                    api_statistic = api_map[api]
-
-                if api_statistic.has_key('num'):
-                    api_statistic['num'] += 1
-                else:
-                    api_statistic['num'] = 1
-                api_map[api] = api_statistic
-                print 'api: ', api, 'api num: ', api_statistic['num']
-                # print 'request id:', current_request_id, 'api: ', api
-
-            match_internal_uri = re.match(r'^.+(\[http://[^\]]+\])', line)
-            if match_internal_uri:
-                pass
-                # print 'request id:', current_request_id, 'internal uri: ', match_internal_uri.group(1)
-
-            match_net_mills = re.match(r'^.+\[net send mills:(\d+)\]', line)
-            if match_net_mills:
-                pass
-                # print 'request id:', current_request_id, 'net mills: ', match_net_mills.group(1)
-
-            match_api_mills = re.match(r'^.+\[api mills:(\d+)\]', line)
-            if match_api_mills:
-                pass
-                # print 'request id:', current_request_id, 'api mills: ', match_api_mills.group(1)
-
-end = datetime.now().microsecond
-
-print 'time elapsed is ', (end - start) / 1000, 'seconds'
-
-print json.dump(api_map, indent=4)
-
+request_count = 0
+with open(out_file_path) as out_file:
+    for line in out_file:
+        request_count += 1
+        request_statistic = {}
+        req_items = line.split('|')
+        request_id = req_items[0]
+        if not request_map.has_key(request_id):
+            request_map[request_id] = {'api': '', 'net_mills': 0, 'api_mills': 0}
+        if req_items[1] == 'api':
+            api = req_items[2].replace('\n', '')
+            if not api_map.has_key(api):
+                api_map[api] = 1
+            else:
+                api_map[api] += 1
+            request_map[request_id]['api'] = req_items[2].replace('\n', '')
+        if req_items[1] == 'net_mills':
+            request_map[request_id]['net_mills'] += int(req_items[2])
+        if req_items[1] == 'api_mills':
+            request_map[request_id]['api_mills'] += int(req_items[2])
+print json.dumps(request_map, indent=4)
+print(request_count)
+print(json.dumps(api_map, indent=4))
