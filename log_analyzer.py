cedc3747429065e4624cc303f09a71db0ce1d6bb
ronniewang <ro_wsy@163.com>
2016-07-21 17:11:57 +0800

    complete in the main

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,8 +1,29 @@
+# encoding: utf-8
 import re
 import time
 import os.path
 import datetime
-import send_email
+import smtplib
+
+STATISTIC_API_CALL_NUM_THRESHOLD = 10
+
+
+def send(sender, password, receivers=[], message=''):
+    try:
+        mail_server = smtplib.SMTP_SSL('smtp.exmail.qq.com', 465)
+        mail_server.login(sender, password)
+        email = """From: {0}
+To: {1}
+Subject: {2}
+
+{3}
+""".format(sender, receivers, '【日志统计】' + yesterday, message)
+        mail_server.sendmail(sender, receivers, email)
+        print "Successfully sent email"
+    except smtplib.SMTPException as e:
+        print e
+        print "Error: unable to send email"
+    mail_server.quit()
 
 
 class ApiStatistic:
@@ -23,11 +44,12 @@ def yesterday_str():
     return (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
 
-start = time.time()
-pwd = '/Users/ronniewang'
-# pwd = os.path.dirname(os.path.realpath(__file__))
-log_file_path = pwd + '/tomcat_kc_web_new_2.' + yesterday_str() + '.log'
-out_file_path = pwd + '/out.log'
+extract_file_start = time.time()
+# pwd = '/Users/ronniewang'
+pwd = os.path.dirname(os.path.realpath(__file__))
+yesterday = yesterday_str()
+log_file_path = pwd + '/tomcat_kc_web_new_2.' + yesterday + '.log'
+out_file_path = pwd + '/simple.log.' + yesterday
 
 if not os.path.isfile(out_file_path):
     out_file = open(out_file_path, 'a+')
@@ -39,19 +61,13 @@ if not os.path.isfile(out_file_path):
                 print line_num
             match_obj = re.match(r'^.+\[requestId:(\d+)\]', line)
             if match_obj:
-                match_api_uri = re.match(r'^.+\[(/[0-9a-zA-Z\./]+)[^\]]+\]', line)
                 current_request_id = match_obj.group(1)
 
+                match_api_uri = re.match(r'^.+\[(/[0-9a-zA-Z\./]+)[^\]]+\]', line)
                 if match_api_uri:
                     api = match_api_uri.group(1)
                     request_str = current_request_id, '|api|', api, '\n'
                     out_file.writelines(request_str)
-
-                    # match_internal_uri = re.match(r'^.+(\[http://[^\]]+\])', line)
-                    # if match_internal_uri:
-                    #     pass
-                    # internal_uri_str = current_request_id, ' internal uri: ', match_internal_uri.group(1), '\n'
-                    # out_file.writelines(internal_uri_str)
 
                 match_net_mills = re.match(r'^.+\[net send mills:(\d+)\]', line)
                 if match_net_mills:
@@ -63,7 +79,10 @@ if not os.path.isfile(out_file_path):
                     api_mills = match_api_mills.group(1)
                     mills_str = current_request_id, '|api_mills|', match_api_mills.group(1), '\n'
                     out_file.writelines(mills_str)
+extract_file_end = time.time()
+print 'extract file time:%fs' % (extract_file_end - extract_file_start)
 
+start = time.time()
 request_map = {}
 api_map = {}
 request_count = 0
@@ -87,29 +106,30 @@ with open(out_file_path) as out_file:
             if api != '':
                 api_map[api]['total_net_mills'] += int(req_items[2])
         if req_items[1] == 'api_mills':
-            request_map[request_id]['api_mills'] += int(req_items[2])
+            request_map[request_id]['api_mills'] = int(req_items[2])
             if api != '':
                 api_map[api]['total_mills'] += request_map[request_id]['api_mills']
+
 # print json.dumps(request_map, indent=4)
-print 'total request: ', request_count
 api_statistics = []
 for k in api_map:
-    if api_map[k]['num'] >= 10:
+    if api_map[k]['num'] >= STATISTIC_API_CALL_NUM_THRESHOLD:
         api_statistics.append(
             ApiStatistic(k, api_map[k]['num'], api_map[k]['total_mills'], api_map[k]['total_net_mills']))
 
-api_statistics = sorted(api_statistics, key=lambda x: x.total_api_num, reverse=True)
+api_statistics = sorted(api_statistics, key=lambda x: x.total_mills / x.total_api_num, reverse=True)
 
 end = time.time()
 
-message_list = ['statistic api num is: {}'.format(len(api_statistics))]
-message_list.append('time elapsed is {}seconds').format(end - start)
+message_list = ['total request num: %d\n' % request_count, 'statistic api num: %d\n' % len(api_statistics),
+                'analyzing time elapsed: %fs\n' % (end - start),
+                '====================================================================================================\n']
 for s in api_statistics:
     message_list.append(''.join([s.api.ljust(40), 'total num:', str(s.total_api_num).ljust(8), 'average mills:',
-                                 str(s.get_average_mills()).ljust(5),
-                                 '\taverage net mills:', str(s.get_average_net_mills()).ljust(5), '\n']))
-
-print 'time elapsed is ', end - start, 'seconds'
+                                 str(s.get_average_mills()).ljust(8),
+                                 'average net mills:', str(s.get_average_net_mills()).ljust(5), '\n']))
 
 print ''.join(message_list)
-send_email.send("***@***.com", '***', ['ro_wsy@163.com'], ''.join(message_list))
+
+import config
+send(config.sender, config.password, config.receivers, ''.join(message_list))
